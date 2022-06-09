a9187ee09f7b04fbc912adfe8dd520a0037e90f5
kienzlemax <102854543+kienzlemax@users.noreply.github.com>
2022-06-09 17:44:48 +0200

    Add files via upload

---
+++ 2_Quellcode/Group.java
@@ -0,0 +1,12 @@
+import javax.swing.JPanel;
+
+public class Group extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Group() {
+
+	}
+
+}
--- 2_Quellcode/Home.java
+++ 2_Quellcode/Home.java
@@ -8,6 +8,8 @@ import java.awt.Color;
 import java.awt.Button;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+
 import java.awt.Font;
 import javax.swing.JPasswordField;
 import javax.swing.ImageIcon;
@@ -26,8 +28,21 @@ public class Home extends JFrame {
 	private JTextField tfNachname;
 	private JTextField tfEMail;
 	private JPasswordField passwordField;
+	private JLabel lblresign;
+    private Home Home;
+    private JLabel lblVorname;
+    private JLabel lblNachname;
+    private JLabel lblEMail;
+    private JLabel lblMatrikelnummer;
+    private Button btnresign;
+    private JLabel lblor;
+    private Button btnSignUp;
+    private Button btnLogin;
+    private JLabel lblresign2;
+    
 	
 	int xx,xy;
+	
 
 	/**
 	 * Launch the application.
@@ -58,6 +73,11 @@ public class Home extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		
+		
+		
+		
 		
 		JPanel panel = new JPanel();
 		panel.setBackground(Color.DARK_GRAY);
@@ -93,10 +113,47 @@ public class Home extends JFrame {
 		lblImage.setVerticalAlignment(SwingConstants.TOP);
 		lblImage.setIcon(new ImageIcon(Home.class.getResource("/images/istockphoto-877267194-170667a (2).jpg")));
 		
-		Button btnSignUp = new Button("SignUp");
+		btnSignUp = new Button("SignUp");
+		btnSignUp.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {				
+				String firstname = tfVorname.getText();
+				String lastname = tfNachname.getText();
+				String password = passwordField.getText();
+				String user = tfEMail.getText();
+				
+				
+				if( password.matches("[+-]?\\d*(\\.\\d+)?") && user.contains("@")) {
+					Home.this.dispose();
+					Login Login = new Login();
+					Login.setUndecorated(true);
+					Login.setVisible(true);	
+					
+				}
+				else lblresign.setText("Matrikelnummer muss aus Zahlen bestehen" );
+				lblresign2.setText("und EMail muss mindestens @ enthalten !");
+				passwordField.setVisible(false);
+				tfEMail.setVisible(false);
+				tfVorname.setVisible(false);
+				tfNachname.setVisible(false);
+				passwordField.setVisible(false);
+				lblEMail.setVisible(false);
+				lblVorname.setVisible(false);
+				lblNachname.setVisible(false);
+				lblMatrikelnummer.setVisible(false);
+				lblor.setVisible(false);
+				btnSignUp.setVisible(false);
+				btnLogin.setVisible(false);
+				btnresign.setVisible(true);
+				
+				
+				}
+				
+				
+			
+		});
 		btnSignUp.setForeground(Color.WHITE);
 		btnSignUp.setBackground(new Color(128, 0, 128));
-		btnSignUp.setBounds(353, 293, 166, 31);
+		btnSignUp.setBounds(352, 302, 166, 31);
 		contentPane.add(btnSignUp);
 		
 		tfVorname = new JTextField();
@@ -104,12 +161,12 @@ public class Home extends JFrame {
 		contentPane.add(tfVorname);
 		tfVorname.setColumns(10);
 		
-		JLabel lblVorname = new JLabel("Vorname");
+		lblVorname = new JLabel("Vorname");
 		lblVorname.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblVorname.setBounds(285, 11, 93, 31);
 		contentPane.add(lblVorname);
 		
-		JLabel lblNachname = new JLabel("Nachname");
+		lblNachname = new JLabel("Nachname");
 		lblNachname.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblNachname.setBounds(285, 82, 234, 24);
 		contentPane.add(lblNachname);
@@ -119,7 +176,7 @@ public class Home extends JFrame {
 		tfNachname.setBounds(285, 109, 280, 31);
 		contentPane.add(tfNachname);
 		
-		JLabel lblEMail = new JLabel("E-Mail");
+		lblEMail = new JLabel("E-Mail");
 		lblEMail.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblEMail.setBounds(285, 151, 46, 14);
 		contentPane.add(lblEMail);
@@ -129,7 +186,7 @@ public class Home extends JFrame {
 		tfEMail.setBounds(285, 176, 280, 31);
 		contentPane.add(tfEMail);
 		
-		JLabel lblMatrikelnummer = new JLabel("Matrikelnummer");
+		lblMatrikelnummer = new JLabel("Matrikelnummer");
 		lblMatrikelnummer.setFont(new Font("Tahoma", Font.PLAIN, 12));
 		lblMatrikelnummer.setBounds(285, 218, 185, 14);
 		contentPane.add(lblMatrikelnummer);
@@ -142,26 +199,63 @@ public class Home extends JFrame {
 		lbl_close.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				
+				if(JOptionPane.showConfirmDialog(Home, "Sind Sie sich sicher ?") ==0 )
 				System.exit(0);
 			}
 		});
 		lbl_close.setForeground(Color.RED);
 		lbl_close.setHorizontalAlignment(SwingConstants.CENTER);
-		lbl_close.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lbl_close.setFont(new Font("Tahoma", Font.BOLD, 18));
 		lbl_close.setBounds(640, 5, 46, 14);
 		contentPane.add(lbl_close);
 		
-		JLabel lblor = new JLabel("or");
+		lblor = new JLabel("or");
 		lblor.setHorizontalAlignment(SwingConstants.CENTER);
-		lblor.setBounds(411, 330, 46, 14);
+		lblor.setBounds(411, 339, 46, 14);
 		contentPane.add(lblor);
 		
-		Button btnLogin = new Button("Login");
+		btnLogin = new Button("Login");
 		btnLogin.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				Home.this.dispose();
+				
+				Login Login = new Login();
+				Login.setUndecorated(true);
+				Login.setVisible(true);
+				
+				
+				
 			}
 		});
-		btnLogin.setBounds(400, 349, 70, 22);
+		btnLogin.setBounds(400, 359, 70, 22);
 		contentPane.add(btnLogin);
+		
+		lblresign = new JLabel("");
+		lblresign.setHorizontalAlignment(SwingConstants.CENTER);
+		lblresign.setBounds(285, 151, 385, 24);
+		contentPane.add(lblresign);
+		
+		btnresign = new Button("Resign");
+		btnresign.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Home.this.dispose();
+				
+				Home Home = new Home();
+				Home.setUndecorated(true);
+				Home.setVisible(true);
+				
+			}
+		});
+		btnresign.setBounds(400, 280, 70, 22);
+		contentPane.add(btnresign);
+		btnresign.setVisible(false);
+		
+		lblresign2 = new JLabel("");
+		lblresign2.setHorizontalAlignment(SwingConstants.CENTER);
+		lblresign2.setBounds(285, 176, 385, 31);
+		contentPane.add(lblresign2);
+		
+		
 	}
 }
