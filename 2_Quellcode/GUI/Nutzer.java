10ee5cfa99cf0c6fc1f9d7825cceea037773e55a
nicom3 <102236743+nicom3@users.noreply.github.com>
2022-06-20 21:33:38 +0200

    Update Nutzer.java

--- 2_Quellcode/GUI/Nutzer.java
+++ 2_Quellcode/GUI/Nutzer.java
@@ -4,24 +4,24 @@ public class Nutzer {
 
 public static void main(String[] args){
 
-    String url = "jdbc:mysql://localhost:3306/todoliste";
+    String url = "jdbc:mysql://localhost:3306/todoliste";       //Lokaler Pfad zum Server
     String user = "root";
     String pass = "";
 
     try {
-        Connection con = DriverManager.getConnection(url, user, pass);
+        Connection con = DriverManager.getConnection(url, user, pass); //Verbindung zum Server
         
-        System.out.println("Erfolgreich mit Datenbank verbunden!");
+        System.out.println("Erfolgreich mit Datenbank verbunden!"); //Kontrollabfrage Verbindung
         
         Statement stm = con.createStatement();
-        String abfrage = "SELECT * FROM user";
+        String abfrage = "SELECT * FROM user"; //SQL-Sprache Zeilen abfragen
         ResultSet rs = stm.executeQuery(abfrage);
 
-        int columns = rs.getMetaData().getColumnCount();
+        int columns = rs.getMetaData().getColumnCount(); //Daten abfragen
         for(int i = 1; i<=columns; i++)
         	System.out.println();
         
-        while(rs.next()){
+        while(rs.next()){                               // Ausgabe der Tabellenzeilen
             System.out.println(rs.getString(1) + " " +
                     rs.getString(2) + " " +
                     rs.getString(3) + " " +
