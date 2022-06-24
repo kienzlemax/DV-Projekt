4fa253b03d3395af9d00ed76bf887d33e34047a8
Felix Fischer <102854516+Fischkopf16@users.noreply.github.com>
2022-06-24 10:36:11 +0200

    Update Todoliste.java

--- 2_Quellcode/GUI/Todoliste.java
+++ 2_Quellcode/GUI/Todoliste.java
@@ -5,6 +5,7 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import java.awt.Color;
+import java.awt.Desktop;
 import java.awt.Dimension;
 
 import javax.swing.JLabel;
@@ -15,8 +16,7 @@ import javax.swing.JButton;
 import com.google.api.services.calendar.Calendar;
 import java.security.GeneralSecurityException;
 import java.io.IOException;
-
-
+import java.net.URL;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
@@ -87,7 +87,7 @@ public class Todoliste extends JFrame {
 		ctnAufgabe1.setLayout(null);
 		
 		JButton btnClearList = new JButton("Clear List");
-		btnClearList.setBounds(304, 39, 122, 20);
+		btnClearList.setBounds(304, 62, 122, 20);
 		ctnAufgabe1.add(btnClearList);
 		btnClearList.addActionListener(new UnserListener8());
 		
@@ -122,7 +122,7 @@ public class Todoliste extends JFrame {
 		lblName = new JLabel("");
 		lblName.setFont(lblName.getFont().deriveFont(lblName.getFont().getStyle() | Font.BOLD,16));
 		lblName.setForeground(new Color(255, 255, 255));
-		lblName.setBounds(10, 0, 350, 40);
+		lblName.setBounds(44, 0, 350, 40);
 		lblName.setHorizontalAlignment(SwingConstants.CENTER);
 		ctnAufgabe1.add(lblName);
 		
@@ -130,129 +130,147 @@ public class Todoliste extends JFrame {
 		lblAufgabe1 = new JLabel("1.");
 		lblAufgabe1.setBackground(Color.DARK_GRAY);
 		lblAufgabe1.setForeground(Color.WHITE);
-		lblAufgabe1.setBounds(10, 98, 312, 13);
+		lblAufgabe1.setBounds(10, 121, 312, 13);
 		ctnAufgabe1.add(lblAufgabe1);
 		
 		
 		lblAufgabe2 = new JLabel("2.");
 		lblAufgabe2.setForeground(Color.WHITE);
-		lblAufgabe2.setBounds(10, 121, 312, 13);
+		lblAufgabe2.setBounds(10, 144, 312, 13);
 		ctnAufgabe1.add(lblAufgabe2);
 		
 		lblAufgabe3 = new JLabel("3.");
 		lblAufgabe3.setForeground(Color.WHITE);
-		lblAufgabe3.setBounds(10, 144, 312, 13);
+		lblAufgabe3.setBounds(10, 167, 312, 13);
 		ctnAufgabe1.add(lblAufgabe3);
 		
 		lblAufgabe4 = new JLabel("4.");
 		lblAufgabe4.setForeground(Color.WHITE);
-		lblAufgabe4.setBounds(10, 168, 312, 13);
+		lblAufgabe4.setBounds(10, 191, 312, 13);
 		ctnAufgabe1.add(lblAufgabe4);
 		
 		lblAufgabe5 = new JLabel("5.");
 		lblAufgabe5.setForeground(Color.WHITE);
-		lblAufgabe5.setBounds(10, 191, 312, 13);
+		lblAufgabe5.setBounds(10, 214, 312, 13);
 		ctnAufgabe1.add(lblAufgabe5);
 		
 		lblAufgabe6 = new JLabel("6.");
 		lblAufgabe6.setForeground(Color.WHITE);
-		lblAufgabe6.setBounds(10, 214, 312, 13);
+		lblAufgabe6.setBounds(10, 237, 312, 13);
 		ctnAufgabe1.add(lblAufgabe6);
 		
 		lblAufgabe7 = new JLabel("7.");
 		lblAufgabe7.setForeground(Color.WHITE);
-		lblAufgabe7.setBounds(10, 237, 312, 13);
+		lblAufgabe7.setBounds(10, 260, 312, 13);
 		ctnAufgabe1.add(lblAufgabe7);
 		
 		txtEingabe = new JTextField("Hier kommen die Aufgaben rein 20.02.2022");
-		txtEingabe.setBounds(10, 69, 416, 19);
+		txtEingabe.setBounds(10, 92, 416, 19);
 		ctnAufgabe1.add(txtEingabe);
 		txtEingabe.setColumns(10);
 		getContentPane().add(txtEingabe);
 		
 		JButton btnAddTask_1 = new JButton("Add");
 		btnAddTask_1.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnAddTask_1.setBounds(375, 98, 51, 13);
+		btnAddTask_1.setBounds(375, 121, 51, 13);
 		ctnAufgabe1.add(btnAddTask_1);
 		btnAddTask_1.addActionListener(new UnserListener1());
 		
 		btnAddTask_2 = new JButton("Add");
 		btnAddTask_2.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnAddTask_2.setBounds(375, 121, 51, 13);
+		btnAddTask_2.setBounds(375, 144, 51, 13);
 		ctnAufgabe1.add(btnAddTask_2);
 		btnAddTask_2.addActionListener(new UnserListener2());
 		
 		btnAddTask_3 = new JButton("Add");
 		btnAddTask_3.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnAddTask_3.setBounds(375, 144, 51, 13);
+		btnAddTask_3.setBounds(375, 167, 51, 13);
 		ctnAufgabe1.add(btnAddTask_3);
 		btnAddTask_3.addActionListener(new UnserListener3());
 		
 		btnAddTask_4 = new JButton("Add");
 		btnAddTask_4.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnAddTask_4.setBounds(375, 168, 51, 13);
+		btnAddTask_4.setBounds(375, 191, 51, 13);
 		ctnAufgabe1.add(btnAddTask_4);
 		btnAddTask_4.addActionListener(new UnserListener4());
 		
 		btnAddTask_5 = new JButton("Add");
 		btnAddTask_5.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnAddTask_5.setBounds(375, 191, 51, 13);
+		btnAddTask_5.setBounds(375, 214, 51, 13);
 		ctnAufgabe1.add(btnAddTask_5);
 		btnAddTask_5.addActionListener(new UnserListener5());
 		
 		btnAddTask_6 = new JButton("Add");
 		btnAddTask_6.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnAddTask_6.setBounds(375, 214, 51, 13);
+		btnAddTask_6.setBounds(375, 237, 51, 13);
 		ctnAufgabe1.add(btnAddTask_6);
 		btnAddTask_6.addActionListener(new UnserListener6());
 		
 		btnAddTask_7 = new JButton("Add");
 		btnAddTask_7.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btnAddTask_7.setBounds(375, 237, 51, 13);
+		btnAddTask_7.setBounds(375, 260, 51, 13);
 		ctnAufgabe1.add(btnAddTask_7);
 		btnAddTask_7.addActionListener(new UnserListener7());
 		
 		JButton btndelet_1 = new JButton("delet");
 		btndelet_1.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btndelet_1.setBounds(319, 98, 58, 13);
+		btndelet_1.setBounds(319, 121, 58, 13);
 		ctnAufgabe1.add(btndelet_1);
 		btndelet_1.addActionListener(new UnserListener9());
 		
 		JButton btndelet_1_1 = new JButton("delet");
 		btndelet_1_1.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btndelet_1_1.setBounds(319, 121, 58, 13);
+		btndelet_1_1.setBounds(319, 144, 58, 13);
 		ctnAufgabe1.add(btndelet_1_1);
 		btndelet_1_1.addActionListener(new UnserListener10());
 		
 		JButton btndelet_1_2 = new JButton("delet");
 		btndelet_1_2.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btndelet_1_2.setBounds(319, 144, 58, 13);
+		btndelet_1_2.setBounds(319, 167, 58, 13);
 		ctnAufgabe1.add(btndelet_1_2);
 		btndelet_1_2.addActionListener(new UnserListener11());
 		
 		JButton btndelet_1_3 = new JButton("delet");
 		btndelet_1_3.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btndelet_1_3.setBounds(319, 168, 58, 13);
+		btndelet_1_3.setBounds(319, 191, 58, 13);
 		ctnAufgabe1.add(btndelet_1_3);
 		btndelet_1_3.addActionListener(new UnserListener12());
 		
 		JButton btndelet_1_4 = new JButton("delet");
 		btndelet_1_4.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btndelet_1_4.setBounds(319, 191, 58, 13);
+		btndelet_1_4.setBounds(319, 214, 58, 13);
 		ctnAufgabe1.add(btndelet_1_4);
 		btndelet_1_4.addActionListener(new UnserListener13());
 		
 		JButton btndelet_1_5 = new JButton("delet");
 		btndelet_1_5.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btndelet_1_5.setBounds(319, 214, 58, 13);
+		btndelet_1_5.setBounds(319, 237, 58, 13);
 		ctnAufgabe1.add(btndelet_1_5);
 		btndelet_1_5.addActionListener(new UnserListener14());
 		
 		JButton btndelet_1_6 = new JButton("delet");
 		btndelet_1_6.setFont(new Font("Tahoma", Font.PLAIN, 9));
-		btndelet_1_6.setBounds(319, 237, 58, 13);
+		btndelet_1_6.setBounds(319, 260, 58, 13);
 		ctnAufgabe1.add(btndelet_1_6);
+		
+		JButton btnOpenKalender = new JButton("Open Kalender");
+		btnOpenKalender.setFont(new Font("Tahoma", Font.PLAIN, 9));
+		btnOpenKalender.setBounds(319, 309, 107, 21);
+		ctnAufgabe1.add(btnOpenKalender);
 		btndelet_1_6.addActionListener(new UnserListener15());
+		
+		btnOpenKalender.addMouseListener(new MouseAdapter (){
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					  Desktop.getDesktop().browse(new URL("https://calendar.google.com/calendar/u/0/r").toURI());
+					} catch (Exception ex) {
+					  ex.printStackTrace();
+					}
+			}
+			
+		});
+
 		
 	
 	}
@@ -274,7 +292,7 @@ public class Todoliste extends JFrame {
                 }
 
 		}
-	}
+			}
 	private class UnserListener2 implements ActionListener {
 
 		@Override
